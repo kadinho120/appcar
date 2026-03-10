a67a9c87053ee437fc6fc5b5e520dc5f2bb9d21a
Rodrigo Neves <rodrigo.neves1995@hotmail.com>
2026-03-10 14:51:47 -0300

    Fix: Remove root from SW cache to avoid auth redirect failure

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'mecanico-v1';
 const ASSETS = [
-    '/',
     '/js/app.js',
     '/icons/icon-192.png',
     '/icons/icon-512.png'
@@ -10,8 +9,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('SW: Pre-caching assets');
-                return cache.addAll(ASSETS);
+                console.log('SW: Pre-caching static assets');
+                return cache.addAll(ASSETS).catch(err => {
+                    console.error('SW: Cache addAll failed. Check if all assets exist and are accessible:', err);
+                });
             })
     );
 });
